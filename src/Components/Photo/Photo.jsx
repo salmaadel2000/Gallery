1437d808a6178016098506ec0504c59507f540e1
salmaadel2000 <salma.adel.fathy77@gmail.com>
2024-06-08 21:26:44 +0300

    Updated Photo component to include Snackbar with smaller close button

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import './Photo.scss';
 
 const Photo = ({ photo, addToWishlistFirestore }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    addToWishlistFirestore(photo);
+    setOpen(true);
+  };
+
+
   return (
     <div className="photo-container">
       <img src={photo.src.medium} alt={photo.alt} />
-      <button onClick={() => addToWishlistFirestore(photo)}>Favorite</button>
+      <button onClick={handleClick}>Favorite</button>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          severity="success"
+          sx={{ width: '100%' }}
+        >
+          Okay, this image has been added to your favorites page.
+        </MuiAlert>
+      </Snackbar>
     </div>
   );
 };
